6996f2afdbca42fd9c2e1b34a5d75ec5f87a92d9
SybrenGL <42715821+SybrenGL@users.noreply.github.com>
2023-03-28 23:16:06 +0200

    Update checkleaderboard.js

--- commands/checkleaderboard.js
+++ commands/checkleaderboard.js
@@ -47,23 +47,3 @@ module.exports = {
     await interaction.reply({ embeds: [embed] });
   }
 };
-
-    .setName("checkleaderboard")
-    .setDescription("Check the top 10 on the leaderboard"),
-  async execute(interaction) {
-    const limit = interaction.options.getInteger("limit") || 10;
-    const leaderboard = await Levels.fetchLeaderboard(
-      interaction.guild.id,
-      limit
-    );
-    const memberPromises = leaderboard.map(async (user, index) => {
-      const member = await interaction.guild.members.fetch(user.userID);
-      return `${index + 1}. ${
-        member.nickname ?? member.user.username
-      } - Level ${user.level} (${user.xp} XP)`;
-    });
-    const leaderboardData = await Promise.all(memberPromises);
-    await interaction.reply(`**Leaderboard: **\n${leaderboardData.join("\n")}`);
-  },
-};
-
