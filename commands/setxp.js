1fd8a0826dad371050eaef23cd7da40ba6627c3b
spooks211 <spooks211@users.noreply.github.com>
2023-04-25 16:28:03 +0000

    Prettified Code!

--- commands/setxp.js
+++ commands/setxp.js
@@ -1,7 +1,7 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const Levels = require('discord-xp');
 
-async function setXP(interaction){
+async function setXP(interaction) {
     let user = interaction.options.getUser('user');
     let amount = interaction.options.getInteger('amount');
 
@@ -12,12 +12,10 @@ async function setXP(interaction){
         true,
     );
 
-    await interaction.reply(
-        `Set ${user}'s XP to ${userTotalXP.xp}.`
-    );
+    await interaction.reply(`Set ${user}'s XP to ${userTotalXP.xp}.`);
 }
 
-module.exports ={
+module.exports = {
     data: new SlashCommandBuilder()
         .setName('setXP')
         .setDescription("Set a user's xp to a specified amount.")
