76e8b02e9d174b7953b47bb5fe1492e8ca46aa96
Sybren Geel <sybren.geel@gmail.com>
2023-05-02 00:18:55 +0200

    Added role assignment when using the commands

--- commands/givexp.js
+++ commands/givexp.js
@@ -1,16 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Levels = require('discord-xp');
+const xp_roles = require('../src/modules/xp_roles.js');
 
 async function giveXP(interaction) {
     let user = interaction.options.getUser('user');
     let amount = interaction.options.getInteger('amount');
 
     await Levels.appendXp(user.id, interaction.guild.id, amount);
-    let userTotalXP = await Levels.fetch(
-        interaction.user.id,
-        interaction.guild.id,
-        true,
-    );
+    let userTotalXP = await Levels.fetch(user.id, interaction.guild.id, true);
+
+    if (userTotalXP) {
+        try {
+            await xp_roles.improvedLevelUp(interaction.guild, user.id);
+        } catch (error) {
+            console.error(error); // add error handling for levelUp functio
+        }
+    }
 
     await interaction.reply(
         `Added ${amount}XP to ${user}. They now have ${userTotalXP.xp} XP!`,
--- commands/setxp.js
+++ commands/setxp.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Levels = require('discord-xp');
+const xp_roles = require('../src/modules/xp_roles.js');
 
 async function setxp(interaction) {
     let user = interaction.options.getUser('user');
@@ -9,12 +10,20 @@ async function setxp(interaction) {
 
     setTimeout(async function () {
         let userTotalXP = await Levels.fetch(
-            interaction.user.id,
+            user.id,
             interaction.guild.id,
             true,
         );
 
         await interaction.reply(`Set ${user}'s XP to ${userTotalXP.xp}.`);
+
+        if (userTotalXP) {
+            try {
+                await xp_roles.improvedLevelUp(interaction.guild, user.id);
+            } catch (error) {
+                console.error(error); // add error handling for levelUp functio
+            }
+        }
     }, 300);
 }
 
