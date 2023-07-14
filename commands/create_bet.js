987cfa72951cf7074612bae2b7ad207c264f1330
Sybren Geel <sybren.geel@gmail.com>
2023-07-14 03:42:36 +0200

    Base done for creating a bet

--- commands/create_bet.js
+++ commands/create_bet.js
@@ -1,5 +1,10 @@
 const { MongoClient } = require('mongodb');
-const { SlashCommandBuilder } = require('discord.js');
+const {
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+} = require('discord.js');
 const uri = `mongodb+srv://admin:x6UPPGjB2JPaTlYG@cluster0.jialcet.mongodb.net/gambling`;
 const client = new MongoClient(uri);
 const db = client.db('gambling');
@@ -31,33 +36,169 @@ module.exports = {
                 .setName('option_2')
                 .setDescription('Option 2 to bet on')
                 .setRequired(true),
+        )
+        .addIntegerOption((option) =>
+            option
+                .setName('time')
+                .setDescription('How long users can bet for (minutes)')
+                .setRequired(true),
         ),
 
     async execute(interaction) {
         const bets = await db.collection('bets');
-
         const name = interaction.options.getString('name');
         const description = interaction.options.getString('description');
         const option_1 = interaction.options.getString('option_1');
         const option_2 = interaction.options.getString('option_2');
+        const time = interaction.options.getInteger('time') * 60 * 1000;
 
-        const doc = {
-            _id: name,
-            description: description,
-            option_1: option_1,
-            option_2: option_2,
-            options: [{ one: option_1, two: option_2 }],
-            xp_option_1: 0,
-            xp_option_2: 0,
-            users_option_1: [],
-            users_option_2: [],
-        };
+        const button_option_1 = new ButtonBuilder()
+            .setCustomId('option_1')
+            .setLabel(option_1)
+            .setStyle(ButtonStyle.Success);
 
-        await bets.insertOne(doc);
+        const button_option_2 = new ButtonBuilder()
+            .setCustomId('option_2')
+            .setLabel(option_2)
+            .setStyle(ButtonStyle.Danger);
 
-        await interaction.reply('Created a bet!');
-        await interaction.channel.send({
-            content: `A bet has been created! The bet is: ${description} \nBet now with with either \n\n/*bet ${name} ${option_1}*\n\nor\n\n*/bet ${name} ${option_2}*`,
+        const row = new ActionRowBuilder().addComponents(
+            button_option_1,
+            button_option_2,
+        );
+
+        let option_1_bets = [];
+        let option_2_bets = [];
+        let option_1_total = 0;
+        let option_2_total = 0;
+        let members_betted = [];
+
+        // Let the user know they created a bet
+        await interaction.reply({ content: 'Created a bet!', ephemeral: true });
+
+        // Send the bet to the channel
+        let bet_message = null;
+
+        interaction.channel
+            .send({
+                content: `A bet has been created! The bet is: ${description} \n The current standing = ${option_1}: ${50}%  - ${option_2}: ${50}%`,
+                components: [row],
+            })
+            .then(async (sent) => {
+                bet_message = sent;
+            });
+
+        // Create collector for button clicks
+        const collector = interaction.channel.createMessageComponentCollector({
+            time: time,
+        });
+
+        collector.on('collect', (collected) => {
+            if (!members_betted.includes(collected.user.id)) {
+                // Prompt the user to enter a number
+                collected.reply({
+                    content: 'Please enter a number:',
+                    ephemeral: true,
+                });
+
+                // Listen for the number entered by the user
+                const numberFilter = (response) =>
+                    response.author.id === collected.user.id;
+
+                const numberCollector =
+                    collected.channel.createMessageCollector({
+                        filter: numberFilter,
+                        time: 30000,
+                    });
+
+                numberCollector.on('collect', (response) => {
+                    // Process the entered number
+                    const enteredNumber = parseInt(response.content);
+                    if (Number.isInteger(enteredNumber)) {
+                        response.reply({
+                            content: `You betted ${enteredNumber}!`,
+                            ephemeral: true,
+                        });
+                        members_betted.push(collected.user.id);
+
+                        console.log(collected.customId);
+
+                        if (collected.customId == 'option_1') {
+                            option_1_total += enteredNumber;
+                            option_1_bets.push({
+                                user: collected.user.id,
+                                amount: enteredNumber,
+                            });
+                        } else {
+                            option_2_total += enteredNumber;
+                            option_2_bets.push({
+                                user: collected.user.id,
+                                amount: enteredNumber,
+                            });
+                        }
+
+                        bet_message
+                            .edit({
+                                content: `A bet has been created! The bet is: ${description} \n The current standing = ${option_1}: **${
+                                    (option_1_total /
+                                        (option_1_total + option_2_total)) *
+                                    100
+                                }%**  - ${option_2}: **${
+                                    (option_2_total /
+                                        (option_1_total + option_2_total)) *
+                                    100
+                                }%**`,
+                                components: [row],
+                            })
+                            .then(console.log('Bet Updated'));
+                    } else {
+                        // Invalid input, prompt the user to enter a valid number
+                        collected.followUp({
+                            content:
+                                'Invalid input. Please enter a valid number.',
+                            ephemeral: true,
+                        });
+                    }
+                    numberCollector.stop();
+                });
+
+                numberCollector.on('end', (collected, reason) => {
+                    if (reason === 'time') {
+                        // No response received within the given time, handle accordingly
+                        interaction.followUp({
+                            content:
+                                'No number entered. Interaction timed out.',
+                            ephemeral: true,
+                        });
+                    }
+                });
+            } else {
+                collected.reply({
+                    content: 'You already betted!',
+                    ephemeral: true,
+                });
+            }
+        });
+
+        collector.on('end', async () => {
+            // Remove the buttons after the interaction ends
+            row.components.forEach((button) => button.setDisabled(true));
+            bet_message.edit({
+                content: 'Betting time has ended',
+                components: [row],
+            });
+
+            const doc = {
+                _id: name,
+                description: description,
+                options: [{ one: option_1, two: option_2 }],
+                xp_option_1: option_1_total,
+                xp_option_2: option_2_total,
+                users_option_1: option_1_bets,
+                users_option_2: option_2_bets,
+            };
+
+            await bets.insertOne(doc);
         });
     },
 };
