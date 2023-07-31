01069557731b1f8b1bc4165d57f8364228b45fc4
spooks211 <spooks211@users.noreply.github.com>
2023-07-31 04:24:45 +0000

    Prettified Code!

--- commands/setfaceitname.js
+++ commands/setfaceitname.js
@@ -1,31 +1,33 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const faceitIntegration = require('../src/modules/faceit_integration');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setfaceitname')
-        .setDescription('Link your discord and faceit name to receive token awards')
+        .setDescription(
+            'Link your discord and faceit name to receive token awards',
+        )
         .addStringOption((option) =>
             option
                 .setName('faceit username')
                 .setDescription('Your faceit username')
-                .setRequired(true)
+                .setRequired(true),
         ),
-        
-        async execute (interaction){
-            let user = interaction.options.getUser('user');
-            let member = 
-                interaction.options.getMember('user') ||
-                (await interaction.guild.members.fetch(user.id));
-            let name = member.nickname || user.username;
 
-            let faceitUsername = interaction.options.getString('faceit username');
-            let allHubMembers = await faceitIntegration.parseNicknames();
+    async execute(interaction) {
+        let user = interaction.options.getUser('user');
+        let member =
+            interaction.options.getMember('user') ||
+            (await interaction.guild.members.fetch(user.id));
+        let name = member.nickname || user.username;
 
-            if (allHubMembers.includes(faceitUsername)) {
-                console.log('Success!');
-            }else{
-                console.log('Fail!');
-            }
-    }
-}
+        let faceitUsername = interaction.options.getString('faceit username');
+        let allHubMembers = await faceitIntegration.parseNicknames();
+
+        if (allHubMembers.includes(faceitUsername)) {
+            console.log('Success!');
+        } else {
+            console.log('Fail!');
+        }
+    },
+};
