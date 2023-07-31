618b7283fe175f934c42821bd20ddd8c1986f235
spooks211 <noah.povil@gmail.com>
2023-07-31 05:22:46 +0100

    end of night (start of morning) push

--- src/modules/faceit_integration.js
+++ src/modules/faceit_integration.js
@@ -4,6 +4,18 @@ const headers = {
     Authorization: 'Bearer 79dc9a0d-0eba-4d1c-8bdc-da36b4ff1b44',
 };
 
+async function parseNicknames (){
+    const faceitData = await getAllHubMembers();
+
+    if (faceitData && faceitData.items) {
+      const nicknameArray = faceitData.items.map(item => item.nickname);
+      console.log(nicknameArray);
+    } else {
+      console.error('Failed to fetch Faceit data or data structure is incorrect.');
+    }
+    return nicknameArray;
+}
+
 async function getHub() {
     let url =
         'https://open.faceit.com/data/v4/hubs/80ee5fb1-0b2b-4c2c-9828-ecf8fc925b12?expanded=game';
@@ -27,26 +39,25 @@ async function getHub() {
 }
 
 async function getAllHubMembers() {
-    let url =
-        'https://open.faceit.com/data/v4/hubs/80ee5fb1-0b2b-4c2c-9828-ecf8fc925b12/members?offset=0&limit=50';
-
-    fetch(url, {
+    let url = 'https://open.faceit.com/data/v4/hubs/80ee5fb1-0b2b-4c2c-9828-ecf8fc925b12/members?offset=0&limit=50';
+  
+    try {
+      const response = await fetch(url, {
         method: 'GET',
         headers: headers,
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('Error: ', error);
-        });
-}
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data; // Return the JSON data
+    } catch (error) {
+      console.error('Error: ', error);
+      return null;
+    }
+  }
 
 async function getAllHubMatches() {
     let url =
@@ -70,8 +81,8 @@ async function getAllHubMatches() {
         });
 }
 
+parseUsernames();
+
 module.exports = {
-    getHub,
-    getAllHubMembers,
-    getAllHubMatches,
+    parseNicknames
 };
