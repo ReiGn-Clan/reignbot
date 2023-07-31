8c69e9dcb6e48a472c2d0ed8950cdcdaab8e735a
spooks211 <spooks211@users.noreply.github.com>
2023-07-31 07:37:44 +0000

    Prettified Code!

--- commands/setfaceitname.js
+++ commands/setfaceitname.js
@@ -1,7 +1,7 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const faceitIntegration = require('../src/modules/faceit_integration');
-const {MongoClient} = require('mongodb');
-const {mongoUris, faceitDbEnvironment} = require('../dev_config.json');
+const { MongoClient } = require('mongodb');
+const { mongoUris, faceitDbEnvironment } = require('../dev_config.json');
 
 const client = new MongoClient(mongoUris[3].faceitDatabase);
 const db = client.db(faceitDbEnvironment);
@@ -10,40 +10,46 @@ const collection = db.collection('usernames');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setfaceitname')
-        .setDescription('Link your discord and faceit name to receive token awards')
+        .setDescription(
+            'Link your discord and faceit name to receive token awards',
+        )
         .addStringOption((option) =>
             option
                 .setName('faceitusername')
                 .setDescription('Your faceit username')
-                .setRequired(true)
+                .setRequired(true),
         ),
-        
-        async execute (interaction){
-            await interaction.deferReply();
-            let faceitUsername = interaction.options.getString('faceitusername');
-            //let allHubMembers = await faceitIntegration.parseNicknames();
-            let discordUsername = interaction.user.username;
 
-            /*if (!allHubMembers.includes(faceitUsername)){
+    async execute(interaction) {
+        await interaction.deferReply();
+        let faceitUsername = interaction.options.getString('faceitusername');
+        //let allHubMembers = await faceitIntegration.parseNicknames();
+        let discordUsername = interaction.user.username;
+
+        /*if (!allHubMembers.includes(faceitUsername)){
                 await interaction.followUp(`${faceitUsername} not found in the FaceIt hub!`);
                 return;
             }*/
 
-            const existingEntry = await collection.findOne({discordUsername});
-            if (existingEntry){
-                const updatedEntry = {$set: {faceitUsername}};
-                await collection.updateOne({discordUsername}, updatedEntry);
-                await interaction.followUp(`Updated ${discordUsername}'s FaceIt username to ${faceitUsername}`);
-            }
+        const existingEntry = await collection.findOne({ discordUsername });
+        if (existingEntry) {
+            const updatedEntry = { $set: { faceitUsername } };
+            await collection.updateOne({ discordUsername }, updatedEntry);
+            await interaction.followUp(
+                `Updated ${discordUsername}'s FaceIt username to ${faceitUsername}`,
+            );
+        }
 
-            if(!existingEntry){
-                // If no entry exists, insert a new data entry into the MongoDB collection
-                const dataEntry = {
-                    discordUsername,
-                    faceitUsername,
-                };
-                await collection.insertOne(dataEntry);
-                await interaction.reply(`Successfully linked ${discordUsername} to ${faceitUsername}`);
-            }
-        }    
-}
+        if (!existingEntry) {
+            // If no entry exists, insert a new data entry into the MongoDB collection
+            const dataEntry = {
+                discordUsername,
+                faceitUsername,
+            };
+            await collection.insertOne(dataEntry);
+            await interaction.reply(
+                `Successfully linked ${discordUsername} to ${faceitUsername}`,
+            );
+        }
+    },
+};
