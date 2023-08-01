ece1478cc08d078f8261e5e6d169de275212135c
Sybren Geel <sybren.geel@gmail.com>
2023-08-01 22:54:42 +0200

    Improved robustness of shop command

--- commands/shop.js
+++ commands/shop.js
@@ -77,142 +77,179 @@ module.exports = {
         });
 
         collector.on('collect', async (collected) => {
-            console.log('Collected');
+            if (collected.message.interaction.id === og_message.id) {
+                console.log('Collected');
 
-            const shop_type = collected.values[0];
+                const shop_type = collected.values[0];
 
-            switch (shop_type) {
-                case 'ranks': {
-                    // Retrieve what the current rank is:
-                    let current_roles = member._roles;
+                switch (shop_type) {
+                    case 'ranks': {
+                        // Retrieve what the current rank is:
+                        let current_roles = member._roles;
 
-                    const current_role_promises = current_roles.map(
-                        async (item) => {
-                            return await interaction.guild.roles.fetch(item);
-                        },
-                    );
-
-                    const current_role_objects = await Promise.all(
-                        current_role_promises,
-                    );
-
-                    const current_role_names = current_role_objects.map(
-                        (role) => role.name,
-                    );
-
-                    let current_rank = current_role_names.filter((x) =>
-                        levelOrder.includes(x),
-                    )[0];
-
-                    const next_rank =
-                        levelOrder[levelOrder.indexOf(current_rank) + 1];
-
-                    // Find the price of the rank in the shop items db
-                    const rank_listing = await shop_items.findOne({
-                        name: next_rank,
-                    });
-
-                    const button_option_1 = new ButtonBuilder()
-                        .setCustomId('yes')
-                        .setLabel('Yes')
-                        .setStyle(ButtonStyle.Success);
-
-                    const button_option_2 = new ButtonBuilder()
-                        .setCustomId('no')
-                        .setLabel('No')
-                        .setStyle(ButtonStyle.Danger);
-
-                    const row_buttons = new ActionRowBuilder().addComponents(
-                        button_option_1,
-                        button_option_2,
-                    );
-
-                    await collected.reply({
-                        content: `You can buy the next rank: ${rank_listing.name} for ${rank_listing.price} ReiGn Tokens.\nWould you like to?`,
-                        ephemeral: true,
-                        components: [row_buttons],
-                    });
-
-                    // Make button collector
-                    const collector_btn =
-                        collected.channel.createMessageComponentCollector({
-                            componentType: ComponentType.Button,
-                            time: 60000,
-                        });
-
-                    collector_btn.on('collect', async (button_collected) => {
-                        row_buttons.components.forEach((button) =>
-                            button.setDisabled(true),
+                        const current_role_promises = current_roles.map(
+                            async (item) => {
+                                return await interaction.guild.roles.fetch(
+                                    item,
+                                );
+                            },
                         );
 
-                        collected.editReply({
-                            content: `You can buy the next rank: ${rank_listing.name} for ${rank_listing.price} ReiGn Tokens.\nWould you like to?`,
-                            components: [row_buttons],
-                            ephemeral: true,
+                        const current_role_objects = await Promise.all(
+                            current_role_promises,
+                        );
+
+                        const current_role_names = current_role_objects.map(
+                            (role) => role.name,
+                        );
+
+                        let current_rank = current_role_names.filter((x) =>
+                            levelOrder.includes(x),
+                        )[0];
+
+                        const next_rank =
+                            levelOrder[levelOrder.indexOf(current_rank) + 1];
+
+                        // Find the price of the rank in the shop items db
+                        const rank_listing = await shop_items.findOne({
+                            name: next_rank,
                         });
 
-                        if (button_collected.customId == 'yes') {
-                            const userXP = await Levels.fetch(
-                                interaction.user.id,
-                                interaction.guild.id,
+                        const button_option_1 = new ButtonBuilder()
+                            .setCustomId('yes')
+                            .setLabel('Yes')
+                            .setStyle(ButtonStyle.Success);
+
+                        const button_option_2 = new ButtonBuilder()
+                            .setCustomId('no')
+                            .setLabel('No')
+                            .setStyle(ButtonStyle.Danger);
+
+                        const row_buttons =
+                            new ActionRowBuilder().addComponents(
+                                button_option_1,
+                                button_option_2,
                             );
 
-                            if (userXP.xp <= rank_listing.price) {
-                                await button_collected.reply({
-                                    content:
-                                        'You do not have enough tokens for this!',
-                                    ephemeral: true,
-                                });
-                                collector_btn.stop();
-                                return;
-                            }
+                        const buy_reply = await collected.reply({
+                            content: `You can buy the next rank: ${rank_listing.name} for ${rank_listing.price} ReiGn Tokens.\nWould you like to?`,
+                            ephemeral: true,
+                            components: [row_buttons],
+                        });
 
-                            await Levels.subtractXp(
-                                interaction.user.id,
-                                interaction.guild.id,
-                                rank_listing.price,
+                        // Make button collector
+                        const collector_btn =
+                            collected.channel.createMessageComponentCollector({
+                                componentType: ComponentType.Button,
+                                time: 60000,
+                            });
+
+                        collector_btn.on(
+                            'collect',
+                            async (button_collected) => {
+                                if (
+                                    buy_reply.interaction.message.id ===
+                                    button_collected.message.reference.messageId
+                                ) {
+                                    row_buttons.components.forEach((button) =>
+                                        button.setDisabled(true),
+                                    );
+
+                                    collected.editReply({
+                                        content: `You can buy the next rank: ${rank_listing.name} for ${rank_listing.price} ReiGn Tokens.\nWould you like to?`,
+                                        components: [row_buttons],
+                                        ephemeral: true,
+                                    });
+
+                                    if (button_collected.customId == 'yes') {
+                                        const userXP = await Levels.fetch(
+                                            interaction.user.id,
+                                            interaction.guild.id,
+                                        );
+
+                                        if (userXP.xp <= rank_listing.price) {
+                                            await button_collected.reply({
+                                                content:
+                                                    'You do not have enough tokens for this!',
+                                                ephemeral: true,
+                                            });
+                                            collector_btn.stop();
+                                            return;
+                                        }
+
+                                        await Levels.subtractXp(
+                                            interaction.user.id,
+                                            interaction.guild.id,
+                                            rank_listing.price,
+                                        );
+
+                                        button_collected.reply({
+                                            content:
+                                                'Thank you for your purchase, processing the new rank!',
+                                            ephemeral: true,
+                                        });
+
+                                        const role =
+                                            await interaction.guild.roles.cache.find(
+                                                (role) =>
+                                                    role.name === next_rank,
+                                            );
+
+                                        const previousRole =
+                                            await interaction.guild.roles.cache.find(
+                                                (role) =>
+                                                    role.name === current_rank,
+                                            );
+
+                                        await member.roles.remove(previousRole);
+                                        await member.roles.add(role);
+                                        collector_btn.stop();
+                                        collector.stop();
+                                    } else {
+                                        await button_collected.reply({
+                                            content: 'Maybe next time!',
+                                            ephemeral: true,
+                                        });
+                                        collector_btn.stop();
+                                        collector.stop();
+                                    }
+                                }
+                            },
+                        );
+                        collector_btn.on('end', async () => {
+                            // Remove the buttons after the interaction ends
+
+                            row_buttons.components.forEach((button) =>
+                                button.setDisabled(true),
                             );
 
-                            button_collected.reply({
-                                content:
-                                    'Thank you for your purchase, processing the new rank!',
+                            await collected.editReply({
+                                content: `You can buy the next rank: ${rank_listing.name} for ${rank_listing.price} ReiGn Tokens.\nWould you like to?`,
+                                components: [row_buttons],
                                 ephemeral: true,
                             });
-
-                            const role =
-                                await interaction.guild.roles.cache.find(
-                                    (role) => role.name === next_rank,
-                                );
-
-                            const previousRole =
-                                await interaction.guild.roles.cache.find(
-                                    (role) => role.name === current_rank,
-                                );
-
-                            await member.roles.remove(previousRole);
-                            await member.roles.add(role);
-                            collector_btn.stop();
-                            collector.stop();
-                        } else {
-                            await button_collected.reply({
-                                content: 'Maybe next time!',
-                                ephemeral: true,
-                            });
-                            collector_btn.stop();
-                            collector.stop();
-                        }
-                    });
-                    break;
-                }
-                case 'items': {
-                    interaction.reply({
-                        content: 'No items in the shop yet!',
-                        ephemeral: true,
-                    });
-                    collector.stop();
-                    break;
+                        });
+                        break;
+                    }
+                    case 'items': {
+                        collected.reply({
+                            content: 'No items in the shop yet!',
+                            ephemeral: true,
+                        });
+                        collector.stop();
+                        break;
+                    }
                 }
             }
+        });
+        collector.on('end', async () => {
+            // Remove the buttons after the interaction ends
+
+            await interaction.editReply({
+                content: 'Select a shop category',
+                ephemeral: true,
+                components: [],
+            });
         });
     },
 };
