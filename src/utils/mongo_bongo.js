e7542633c032a2dc22d00e8fb102857d6eb80320
Sybren Geel <sybren.geel@gmail.com>
2023-08-01 19:03:32 +0200

    More efficient Bongo'ing

---
+++ src/utils/mongo_bongo.js
@@ -0,0 +1,37 @@
+// db.js
+// pass RaUEOQOMA9ArGg2T
+// SYB: mongodb+srv://SybrenGL:RaUEOQOMA9ArGg2T@cluster0.s2hbjjn.mongodb.net/?retryWrites=true&w=majority
+// REIGN: mongodb+srv://admin:vZxUHrWiAWpVNVOd@cluster0.jialcet.mongodb.net/
+
+const { MongoClient } = require('mongodb');
+
+const url =
+    'mongodb+srv://admin:vZxUHrWiAWpVNVOd@cluster0.jialcet.mongodb.net/';
+
+let client = null;
+
+function connectToDatabase() {
+    console.log('We are here');
+    try {
+        client = new MongoClient(url);
+
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+function getDbInstance(db_name) {
+    console.log('Retrieving DB', db_name);
+    if (client == null) {
+        console.log('Client is null');
+        return;
+    }
+    const db = client.db(db_name);
+    if (db == null) {
+        console.log('error retrieving db');
+    }
+    return db;
+}
+
+module.exports = { connectToDatabase, getDbInstance };
