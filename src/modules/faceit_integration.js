ec41e802b24fac0107b2d1580aeb6b0cc79383af
spooks211 <noah.povil@gmail.com>
2023-08-03 19:19:51 +0100

    shit and fuck

--- src/modules/faceit_integration.js
+++ src/modules/faceit_integration.js
@@ -1,9 +1,19 @@
 const fetch = require('cross-fetch');
-const { faceitJsonAccept, faceitAuth } = require('../../dev_config.json');
+const { faceitJsonAccept, faceitAuth, faceitDbEnvironment, discordAPIBotStuff, xpDbEnvironment } = require('../../dev_config.json');
 const headers = {
     accept: faceitJsonAccept,
     Authorization: faceitAuth,
 };
+
+const mongo_bongo = require('../utils/mongo_bongo.js');
+const Levels = require('../utils/syb_xp.js');
+const xp_roles = require('../modules/xp_roles.js');
+
+mongo_bongo.connectToDatabase();
+Levels.set_collection(xpDbEnvironment, 'levels');
+const db = mongo_bongo.getDbInstance(faceitDbEnvironment);
+const usernamesCollection = db.collection('usernames');
+const awardedMatchesCollection = db.collection('awardedMatches');
 
 async function parseNicknames() {
     const faceitData = await getAllHubMembers();
@@ -20,26 +30,59 @@ async function parseNicknames() {
     return nicknameArray;
 }
 
-async function getHub() {
-    let url =
-        'https://open.faceit.com/data/v4/hubs/80ee5fb1-0b2b-4c2c-9828-ecf8fc925b12?expanded=game';
-
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: headers,
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data; // Return the JSON data
-    } catch (error) {
-        console.error('Error: ', error);
-        return null;
+async function rewardParticipants (){
+    let {nicknames, matchID} = await parseMatches();
+    const alreadyAwarded = await awardedMatchesCollection.findOne({matchID});
+    if (alreadyAwarded) {
+        console.log(`MatchID: ${matchID} has already been awarded!`);
+        return;
     }
+
+    if (!alreadyAwarded){
+        try {
+            // Use MongoDB driver to find players in the database based on lowercase nicknames
+            const linkedUsernames = await usernamesCollection.find({ faceitUsername: { $in: nicknames } }).toArray();
+            for (const player of linkedUsernames) {
+                if (player.discordUsername) {
+                    let hasLeveledUp = await Levels.appendXp(
+                        player.discordUsername,
+                        discordAPIBotStuff[1].guildID,
+                        5000,
+                        console.log('Awarded xp for match!')
+                    );
+
+                    if (hasLeveledUp) {
+                        try{
+                            await xp_roles.improvedLevelUp(
+                                discordAPIBotStuff[1].guildID,
+                                player.discordUsername,
+                                discordClient,
+                            );
+                        } catch(error){
+                            console.error(error);
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            console.error("Error while querying the database:", error);
+        }
+    }
+}
+
+async function parseMatches(){
+    const matchData = await getAllHubMatches();
+    let team1 = [];
+    let team2 = [];
+    team1 = matchData.items[0].teams.faction1.roster;
+    team2 = matchData.items[0].teams.faction2.roster;
+    
+    let fullRoster = team1.concat(team2);
+    let nicknamesRaw =  fullRoster.map(player => player.nickname);
+    let nicknames = nicknamesRaw.map(nickname => nickname.toLowerCase());
+    let matchID = matchData.items[0].match_id;
+
+    return {nicknames, matchID};
 }
 
 async function getAllHubMembers() {
@@ -88,8 +131,30 @@ async function getAllHubMatches() {
     }
 }
 
+async function getHub() {
+    let url =
+        'https://open.faceit.com/data/v4/hubs/80ee5fb1-0b2b-4c2c-9828-ecf8fc925b12?expanded=game';
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: headers,
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data; // Return the JSON data
+    } catch (error) {
+        console.error('Error: ', error);
+        return null;
+    }
+}
+
 module.exports = {
+    parseMatches,
     parseNicknames,
-    getHub,
-    getAllHubMatches,
+    rewardParticipants
 };
