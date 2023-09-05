e06af29a123cafdb4c146de9a4506fe96e24fa77
maeeljavanar <maeeljavanar@users.noreply.github.com>
2023-09-05 23:52:50 +0000

    Prettified Code!

--- commands/convertunits.js
+++ commands/convertunits.js
@@ -1,31 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 async function convertUnits(interaction) {
-    
-
     let source = interaction.options.getString('value');
     let value;
 
-    if(source.includes("'") || source.includes('ft') || source.includes('feet')) {
-
+    if (
+        source.includes("'") ||
+        source.includes('ft') ||
+        source.includes('feet')
+    ) {
         //turn to x'y
         source = source.replace(/ft/g, "'");
         source = source.replace(/feet/g, "'");
-        
+
         let trimmed = source.replace(/[^0-9']/g, '');
         let sourceVals = trimmed.split("'");
 
         value = sourceVals[0] / 3.2808399;
         value += sourceVals[1] / 39.3700787;
         value = value.toFixed(2);
-        value += " meters";
-
+        value += ' meters';
     } else {
         //if cm just remove units
         if (source.includes('cm')) {
             source = source.replace(/\D/g, '');
 
-        //if m turn to cm and remove units
+            //if m turn to cm and remove units
         } else if (source.includes('m') || source.includes('meters')) {
             let trimmed = source.replace(/[^0-9.]/g, '');
             source = trimmed * 100;
@@ -40,16 +40,17 @@ async function convertUnits(interaction) {
         value = `${feet}'${inches}`;
     }
 
-    interaction.reply({ content: `Converted value: ${value}`, ephemeral: true })
-
+    interaction.reply({
+        content: `Converted value: ${value}`,
+        ephemeral: true,
+    });
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('convert')
-        .setDescription(
-            'Convert between metric and imperial',
-        ).addStringOption((option) =>
+        .setDescription('Convert between metric and imperial')
+        .addStringOption((option) =>
             option
                 .setName('value')
                 .setDescription('Value to be converted')
