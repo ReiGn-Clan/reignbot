46a7146901c02dedc16a17442c5c07febaaaf1d5
spooks211 <noah.povil@gmail.com>
2023-09-12 16:04:33 +0100

    added checking for topgg rewards

--- src/modules/topgg_integration.js
+++ src/modules/topgg_integration.js
@@ -4,39 +4,42 @@ const xp_roles = require('./xp_roles.js');
 const { discordAPIBotStuff, variousIDs } = require(`../../${config_to_use}`);
 
 let discordClient;
-
 function setClient(client) {
     discordClient = client;
 }
 
 async function rewardVote(user) {
-    let hasLeveledUp = await Levels.appendXp(
-        user,
-        discordAPIBotStuff[1].guildID,
-        1500,
-        console.log('Awarded tokens for voting!'),
-    );
-    const channel = await discordClient.channels.fetch(
-        variousIDs[0].userUpdatesChannel,
-    );
-
-    const guild = await discordClient.guilds.fetch(
-        discordAPIBotStuff[1].guildID,
-    );
     const member = await guild.members.fetch(user);
-    await channel.send({
-        content: `${member.user} has earned **1500** ReiGn Tokens for voting on https://top.gg/servers/1089665371923026053 !`,
-    });
+    if (!member) {
+        console.log('User is not in the server!');
+        return;
+    } else {
+            let hasLeveledUp = await Levels.appendXp(
+            user,
+            discordAPIBotStuff[1].guildID,
+            1500,
+            console.log('Awarded tokens for voting!'),
+        );
+        const channel = await discordClient.channels.fetch(
+            variousIDs[0].userUpdatesChannel,
+        );
 
-    if (hasLeveledUp) {
-        try {
-            await xp_roles.improvedLevelUp(guild, user, discordClient);
-        } catch (error) {
-            console.error(error);
+        const guild = await discordClient.guilds.fetch(
+            discordAPIBotStuff[1].guildID,
+        );
+        await channel.send({
+            content: `${member.user} has earned **1500** ReiGn Tokens for voting on https://top.gg/servers/1089665371923026053 !`,
+        });
+
+        if (hasLeveledUp) {
+            try {
+                await xp_roles.improvedLevelUp(guild, user, discordClient);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
-
-    // Start reminder for in 12 hours
+ // Start reminder for in 12 hours
 }
 
 module.exports = { setClient, rewardVote };
