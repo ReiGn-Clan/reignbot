a442581441b23a9ea3e2ce7a7c3783a09196fe43
Sybren Geel <sybren.geel@gmail.com>
2023-09-12 18:57:00 +0200

    Reminder test

--- src/modules/topgg_integration.js
+++ src/modules/topgg_integration.js
@@ -9,37 +9,51 @@ function setClient(client) {
 }
 
 async function rewardVote(user) {
+    const guild = await discordClient.guilds.fetch(
+        discordAPIBotStuff[1].guildID,
+    );
     const member = await guild.members.fetch(user);
-    if (!member) {
+
+    if (member === undefined) {
         console.log('User is not in the server!');
         return;
-    } else {
-        let hasLeveledUp = await Levels.appendXp(
-            user,
-            discordAPIBotStuff[1].guildID,
-            1500,
-            console.log('Awarded tokens for voting!'),
-        );
-        const channel = await discordClient.channels.fetch(
-            variousIDs[0].userUpdatesChannel,
-        );
+    }
 
-        const guild = await discordClient.guilds.fetch(
-            discordAPIBotStuff[1].guildID,
-        );
-        await channel.send({
-            content: `${member.user} has earned **1500** ReiGn Tokens for voting on https://top.gg/servers/1089665371923026053 !`,
-        });
+    let hasLeveledUp = await Levels.appendXp(
+        user,
+        discordAPIBotStuff[1].guildID,
+        1500,
+        console.log('Awarded tokens for voting!'),
+    );
+    const channel = await discordClient.channels.fetch(
+        variousIDs[0].userUpdatesChannel,
+    );
 
-        if (hasLeveledUp) {
-            try {
-                await xp_roles.improvedLevelUp(guild, user, discordClient);
-            } catch (error) {
-                console.error(error);
-            }
+    await channel.send({
+        content: `${member.user} has earned **1500** ReiGn Tokens for voting on https://top.gg/servers/1089665371923026053 !`,
+    });
+
+    if (hasLeveledUp) {
+        try {
+            await xp_roles.improvedLevelUp(guild, user, discordClient);
+        } catch (error) {
+            console.error(error);
         }
     }
+
     // Start reminder for in 12 hours
+
+    // First check if users has role
+
+    const hasRole = member.roles.cache.has('1151195100089688154');
+
+    if (hasRole) {
+        setTimeout(() => {
+            channel.send({
+                content: `${member.user} It's time to vote again on https://top.gg/servers/1089665371923026053 !`,
+            });
+        }, 10);
+    }
 }
 
 module.exports = { setClient, rewardVote };
