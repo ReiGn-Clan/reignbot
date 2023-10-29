639c757e301bb00c7dec60aa65b50a6f1a9887bb
spooks211 <spooks211@users.noreply.github.com>
2023-10-29 23:00:42 +0000

    Prettified Code!

--- commands/rockpaperscissors.js
+++ commands/rockpaperscissors.js
@@ -11,7 +11,7 @@ const db = mongo_bongo.getDbInstance(gamblingDbEnvironment);
 const Levels = require('../src/utils/syb_xp.js');
 const xp_roles = require('../src/modules/xp_roles.js');
 
-async function rockPaperScissors(interaction){
+async function rockPaperScissors(interaction) {
     const tokens = interaction.options.getInteger('tokens');
     const user = interaction.options.getUser('user');
     const gambles = db.collection('gambles');
@@ -32,22 +32,22 @@ async function rockPaperScissors(interaction){
     const button_option_rock = new ButtonBuilder()
         .setCustomId('rock')
         .setLabel('Rock')
-        .setStyle(ButtonStyle.Primary)
+        .setStyle(ButtonStyle.Primary);
 
     const button_option_paper = new ButtonBuilder()
         .setCustomId('paper')
         .setLabel('Paper')
-        .setStyle(ButtonStyle.Secondary)
+        .setStyle(ButtonStyle.Secondary);
 
     const button_option_scissors = new ButtonBuilder()
         .setCustomId('scissors')
         .setLabel('Scissors')
-        .setStyle(ButtonStyle.Success)
+        .setStyle(ButtonStyle.Success);
 
     const row = new ActionRowBuilder().addComponents(
         button_option_rock,
         button_option_paper,
-        button_option_scissors
+        button_option_scissors,
     );
 
     await interaction.reply({
@@ -62,15 +62,13 @@ async function rockPaperScissors(interaction){
     if (user == null) {
         await interaction.channel
             .send({
-                content : `${
+                content: `${
                     interaction.user
                 } create a game of rock paper scissors! You are betting for ${tokens} ReiGn Tokens! \n The winnner will take home **${(
                     tokens *
                     2 *
                     0.95
-                ).toFixed(
-                    0,
-                )}** ReiGn Tokens \nYou have 60 seconds to respond!`,
+                ).toFixed(0)}** ReiGn Tokens \nYou have 60 seconds to respond!`,
                 components: [row],
             })
             .then(async (sent) => {
@@ -102,7 +100,6 @@ async function rockPaperScissors(interaction){
         collector = interaction.channel.createMessageComponentCollector({
             time: 60000,
         });
-    
     } else {
         const collectorFilter = async (i) => {
             if (
@@ -131,34 +128,38 @@ async function rockPaperScissors(interaction){
     collector.on('collect', async (collected) => {
         const userXP = await Levels.fetch(
             collected.user.id,
-            interaction.guild.id
+            interaction.guild.id,
         );
 
-        const alreadyInArray = choiceArray.some(obj => obj.userID === collected.user.id);
+        const alreadyInArray = choiceArray.some(
+            (obj) => obj.userID === collected.user.id,
+        );
 
-        if (userXP.xp >= tokens && alreadyInArray === false && choiceArray.length < 2){
+        if (
+            userXP.xp >= tokens &&
+            alreadyInArray === false &&
+            choiceArray.length < 2
+        ) {
             const choice = {
                 userID: collected.user.id,
-                rps: collected.customId //rps = rock paper scissors (which one of those the user picked)
+                rps: collected.customId, //rps = rock paper scissors (which one of those the user picked)
             };
             choiceArray.push(choice);
 
             await collected.reply({
                 content: `You've chosen **${collected.customId}**!`,
-                ephemeral: true
+                ephemeral: true,
             });
-
-            } else {
-                await collected.reply({
-                    content: "You either don't have enough tokens for this or you've already made a choice!",
-                    ephemeral: true
+        } else {
+            await collected.reply({
+                content:
+                    "You either don't have enough tokens for this or you've already made a choice!",
+                ephemeral: true,
             });
         }
-            
-        if (choiceArray.length === 2){
-            row.components.forEach((button) =>
-                button.setDisabled(true),
-            );
+
+        if (choiceArray.length === 2) {
+            row.components.forEach((button) => button.setDisabled(true));
 
             await bet_message
                 .edit({
@@ -175,9 +176,9 @@ async function rockPaperScissors(interaction){
                     bet_message = sent;
                 });
 
-            switch(choiceArray[0].rps) {
+            switch (choiceArray[0].rps) {
                 case 'rock':
-                    switch(choiceArray[1].rps) {
+                    switch (choiceArray[1].rps) {
                         case 'rock':
                             winner = 'tie';
                             break;
@@ -207,7 +208,7 @@ async function rockPaperScissors(interaction){
                             break;
                     }
                     break;
-                    
+
                 case 'scissors':
                     switch (choiceArray[1].rps) {
                         case 'rock':
@@ -225,97 +226,98 @@ async function rockPaperScissors(interaction){
                     break;
                 default:
                     console.log('Something went wrong!');
-                }
-                if (winner == 'tie'){
-                    const player1Member = 
-                        await interaction.guild.members.fetch(
-                            choiceArray[0].userID
-                    );
-            
-                    const player2Member = 
-                        await interaction.guild.members.fetch(
-                            choiceArray[1].userID
-                    );
-                    
-                    await interaction.channel.send({
-                        content: `It's a tie! Both ${player1Member.user} and ${player2Member.user} chose ***${choiceArray[0].rps}***.\nYour **ReiGn Tokens** have been refunded.\nUse */rockpaperscissors* again if you'd like a rematch!`
-                    });
-
-                } else {
-                    const winnerMember = await interaction.guild.members.fetch(
-                            winner.userID
-                        );
-
-                    const loserMember = await interaction.guild.members.fetch(
-                        loser.userID
-                    );
-                    
-                    await interaction.channel.send({
-                        content: `${winnerMember.user} chose ${winner.rps} and ${loserMember.user} chose ${loser.rps}.\n${winnerMember.user} wins **${tokens * 2 * 0.95} ReiGn Tokens**!`
-                    });
-
-                    await Levels.subtractXp(
-                        winnerMember.user.id,
-                        interaction.guild.id,
-                        tokens
-                    ); //take away original bet amount from winner so the maths works out
-
-                    const hasLeveledUp = await Levels.appendXp(
-                        winnerMember.user.id,
-                        interaction.guild.id,
-                        tokens * 2 * 0.95,
-                    );
-
-                    const hasLeveledDown = await Levels.subtractXp(
-                        loserMember.user.id,
-                        interaction.guild.id,
-                        tokens
-                    );
-
-                    if (hasLeveledUp){
-                        try{
-                            await xp_roles.improvedLevelUp(
-                                interaction.guild,
-                                winnerMember.user.id,
-                                interaction.client,
-                                false,
-                                true
-                            );
-                        } catch (error){
-                            console.error(error);
-                        }
-                    }
-                    if (hasLeveledDown){
-                        try{
-                            xp_roles.improvedLevelUp(
-                                interaction.guild,
-                                loserMember.user.id,
-                                interaction.client,
-                                true,
-                                true,
-                            );
-                        } catch (error){
-                            console.error(error);
-                        }
-                    }
-                    const doc = {
-                        game: 'rockpaperscissors',
-                        starter: interaction.user.id,
-                        result: winner.rps,
-                        winner: winner.userID,
-                        loser: loser.userID,
-                        stake: tokens,
-                        payout: tokens * 2 * 0.95,
-                    };
-                    timeout = false;
-                    await gambles.insertOne(doc);
-                    collector.stop();
-                }
             }
+            if (winner == 'tie') {
+                const player1Member = await interaction.guild.members.fetch(
+                    choiceArray[0].userID,
+                );
+
+                const player2Member = await interaction.guild.members.fetch(
+                    choiceArray[1].userID,
+                );
+
+                await interaction.channel.send({
+                    content: `It's a tie! Both ${player1Member.user} and ${player2Member.user} chose ***${choiceArray[0].rps}***.\nYour **ReiGn Tokens** have been refunded.\nUse */rockpaperscissors* again if you'd like a rematch!`,
+                });
+            } else {
+                const winnerMember = await interaction.guild.members.fetch(
+                    winner.userID,
+                );
+
+                const loserMember = await interaction.guild.members.fetch(
+                    loser.userID,
+                );
+
+                await interaction.channel.send({
+                    content: `${winnerMember.user} chose ${winner.rps} and ${
+                        loserMember.user
+                    } chose ${loser.rps}.\n${winnerMember.user} wins **${
+                        tokens * 2 * 0.95
+                    } ReiGn Tokens**!`,
+                });
+
+                await Levels.subtractXp(
+                    winnerMember.user.id,
+                    interaction.guild.id,
+                    tokens,
+                ); //take away original bet amount from winner so the maths works out
+
+                const hasLeveledUp = await Levels.appendXp(
+                    winnerMember.user.id,
+                    interaction.guild.id,
+                    tokens * 2 * 0.95,
+                );
+
+                const hasLeveledDown = await Levels.subtractXp(
+                    loserMember.user.id,
+                    interaction.guild.id,
+                    tokens,
+                );
+
+                if (hasLeveledUp) {
+                    try {
+                        await xp_roles.improvedLevelUp(
+                            interaction.guild,
+                            winnerMember.user.id,
+                            interaction.client,
+                            false,
+                            true,
+                        );
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+                if (hasLeveledDown) {
+                    try {
+                        xp_roles.improvedLevelUp(
+                            interaction.guild,
+                            loserMember.user.id,
+                            interaction.client,
+                            true,
+                            true,
+                        );
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+                const doc = {
+                    game: 'rockpaperscissors',
+                    starter: interaction.user.id,
+                    result: winner.rps,
+                    winner: winner.userID,
+                    loser: loser.userID,
+                    stake: tokens,
+                    payout: tokens * 2 * 0.95,
+                };
+                timeout = false;
+                await gambles.insertOne(doc);
+                collector.stop();
+            }
+        }
     });
 
     collector.on('end', async () => {
-        if (timeout === true){
+        if (timeout === true) {
             row.components.forEach((button) => button.setDisabled(true));
 
             bet_message
@@ -325,7 +327,7 @@ async function rockPaperScissors(interaction){
                 })
                 .then(async (sent) => {
                     bet_message = sent;
-            });
+                });
         }
     });
 }
@@ -341,11 +343,11 @@ module.exports = {
                 .setRequired(true)
                 .setMinValue(1),
         )
-        .addUserOption((option) => 
+        .addUserOption((option) =>
             option
                 .setName('user')
                 .setDescription('Do you want to challenge a specific user?')
                 .setRequired(false),
         ),
     execute: rockPaperScissors,
-}
+};
