e116542828a7cda0fc0365db01ad3d84097ee2e8
spooks211 <spooks211@users.noreply.github.com>
2023-10-31 16:02:56 +0000

    Prettified Code!

--- src/modules/challengermode_integration.js
+++ src/modules/challengermode_integration.js
@@ -1,2 +1,4 @@
-const {config_to_use} = require('../../general_config.json');
-const {discordAPIBotStuff, variousIDs, CMAuth} = require(`../../${config_to_use}`);
+const { config_to_use } = require('../../general_config.json');
+const { discordAPIBotStuff, variousIDs, CMAuth } = require(
+    `../../${config_to_use}`,
+);
