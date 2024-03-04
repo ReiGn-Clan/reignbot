fd8589c4f181d9d4df557af25e84ad5fab50510e
Sybren Geel <sybren.geel@gmail.com>
2024-03-04 22:50:47 +0100

    Twitch rework, need to figure out HTTPS shit

--- src/utils/webhookserver.js
+++ src/utils/webhookserver.js
@@ -4,6 +4,7 @@ const { webserverPort } = require(`../../${config_to_use}`);
 const app = express();
 const faceit_integration = require('../modules/faceit_integration.js');
 const topgg_integration = require('../modules/topgg_integration.js');
+const twitch_integration = require('../modules/twitch_integration.js');
 const Topgg = require('@top-gg/sdk');
 const webhook = new Topgg.Webhook('r31gn0nt0p');
 
@@ -14,6 +15,23 @@ function startWebHookServer() {
         const matchData = req.body;
         console.log('Received webhook notification');
         faceit_integration.rewardParticipants(matchData);
+        res.sendStatus(200);
+    });
+
+    app.post('/webhook/callback', (req, res) => {
+        const { challenge } = req.body;
+        // Twitch sends a challenge query param to verify your endpoint
+        if (challenge) {
+            return res.status(200).send(challenge);
+        }
+
+        const { subscription, event } = req.body;
+
+        twitch_integration.handleEventsub(
+            subscription.type,
+            event.broadcaster_user_name,
+        );
+
         res.sendStatus(200);
     });
 
