cb01b2a489188853d51e5caf233e9cbfaae1573b
spooks211 <noah.povil@gmail.com>
2024-03-09 17:23:13 +0000

    oops

--- src/modules/twitch_integration.js
+++ src/modules/twitch_integration.js
@@ -104,6 +104,95 @@ async function handleEventsub(eventType, broadcasterName) {
     }
 }
 
+async function handleGoLive(whichStreamer) {
+    const collection = db.collection('streamers');
+    const guild = await discordClient.guilds.cache.get(
+        discordAPIBotStuff[1].guildID,
+    );
+    const liveRole = await guild.roles.cache.get('1157008708165959680');
+    const channel = await discordClient.channels.fetch(
+        variousIDs[5].socialUpdatesChannel,
+    );
+
+    let member = null;
+    let messageObj = {
+        followerMention: null,
+        memberToPing: null,
+        streamLink: null,
+    };
+
+    const data = await collection.findOne({ twitchUsername: whichStreamer });
+
+    console.log(data);
+
+    member = await guild.members.fetch(data.userId);
+    messageObj.followerMention = `<@&${data.roleId}>`;
+    messageObj.memberToPing = data.userId;
+    messageObj.streamLink = `https://www.twitch.tv/${whichStreamer}`;
+
+    const hasRole = member.roles.cache.some(
+        (role) => role.name === liveRole.name,
+    );
+    if (hasRole) {
+        console.log('User already has live role! Skipping message');
+        return;
+    } else {
+        await channel.send(
+            `${messageObj.followerMention}, <@${messageObj.memberToPing}> has gone live! Check out their stream at ${messageObj.streamLink}`,
+        );
+    }
+
+    member.roles
+        .add(liveRole)
+        .then(() => {
+            console.log(
+                `Added role ${liveRole.name} to ${messageObj.memberToPing}.`,
+            );
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+async function handleGoOffline(whichStreamer) {
+    const collection = db.collection('streamers');
+    const guild = await discordClient.guilds.cache.get(
+        discordAPIBotStuff[1].guildID,
+    );
+    const liveRole = await guild.roles.cache.get('1157008708165959680');
+
+    const data = await collection.findOne({ twitchUsername: whichStreamer });
+
+    let member = null;
+
+    member = await guild.members.fetch(data.userId);
+
+    const hasRole = member.roles.cache.some(
+        (role) => role.name === liveRole.name,
+    );
+    if (hasRole) {
+        member.roles.remove(liveRole);
+        console.log(`Removed role ${liveRole.name} from ${whichStreamer}`);
+    } else {
+        console.log(`${whichStreamer} doesn't have LIVE role, skipping.`);
+        return;
+    }
+}
+
+// Function to create all the initial subscriptions to events
+async function botStartup() {
+    await authenticate();
+    await deleteAllSubscriptions();
+    const collection = db.collection('streamers');
+    // Find all documents in the collection
+    const allDocs = await collection.find({}).toArray(); // Converts to array to iterate
+
+    // Iterate over each document
+    allDocs.forEach(async (doc) => {
+        await subscribeOnlineOffline(doc.twitchUserId);
+    });
+}
+
 module.exports = {
     setClient,
     handleEventsub,
