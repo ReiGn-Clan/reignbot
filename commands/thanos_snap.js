14042324f0184da972446481debb305067cc72d7
spooks211 <spooks211@users.noreply.github.com>
2024-05-25 11:01:15 +0000

    Prettified Code!

--- commands/thanos_snap.js
+++ commands/thanos_snap.js
@@ -2,22 +2,29 @@ const { SlashCommandBuilder } = require('discord.js');
 const Levels = require('../src/utils/syb_xp.js');
 const roles = require('../json/levelNames.json');
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function thanosSnap(interaction) {
     const guild = interaction.guild;
     const members = await guild.members.fetch();
-    const neophyteRole = guild.roles.cache.find(role => role.name === 'Neophyte');
+    const neophyteRole = guild.roles.cache.find(
+        (role) => role.name === 'Neophyte',
+    );
 
-    const roleNames = roles.ranges.map(range => range.value);
+    const roleNames = roles.ranges.map((range) => range.value);
 
     members.forEach(async (member) => {
         await sleep(350); // to avoid rate limit
         roleNames.forEach(async (roleName) => {
-            if (roleName !== 'Neophyte' && await member.roles.cache.has(roleName)) {
+            if (
+                roleName !== 'Neophyte' &&
+                (await member.roles.cache.has(roleName))
+            ) {
                 await member.roles.remove(roleName);
                 await member.roles.add(neophyteRole);
-                console.log(`Removed ${roleName} from ${member.user.username}. Set to Neophyte.`);
+                console.log(
+                    `Removed ${roleName} from ${member.user.username}. Set to Neophyte.`,
+                );
             }
         });
         await Levels.setXp(member.id, guild.id, 0);
